21c53f0025d3e3b1f644d54550b95ecdd40bf0ee
ammar <ammarsleem321@gamil.com>
2022-12-05 20:40:56 +0200

    First

---
+++ task1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>  
+ 
+ int main() {  
+    float r, area,  pi = 3.14;  
+    printf("Enter the radius of circle: ");  
+    scanf("%f",&r);   
+    area = pi*r*r;   
+    printf("The area of the circle: %f",area);  
+      
+   return 0;   
+} 
