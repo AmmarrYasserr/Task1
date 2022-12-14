cd585e5cf6f036ce2a9fc7c962ae679b321f558e
ammar <ammarsleem321@gamil.com>
2022-12-14 19:17:22 +0200

    Calculator

---
+++ calc.dart
@@ -0,0 +1,13 @@
+void main () {
+  
+double x = 20.5;
+double y = 5.5;
+
+double add   = x+y;
+double sub   = x-y;
+double multi = x*y;
+double div   = x/y;
+
+print("Addition:       "+"$add \n"+"Subtract:       "+"$sub \n"+"Multiplication: "+"$multi \n"+"Division:       "+"$div \n");
+
+}  
